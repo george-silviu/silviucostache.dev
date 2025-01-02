717e7491c5dcfee0cb676676be3e9fd5b74a8444
unknown <costache.georgesilviu@yahoo.ro>
2025-01-02 21:22:12 +0200

    uploading project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <div className="main"> Hello to this awesome website</div>;
+}
+
+export default App;
